c36ae8809bdea51118e28d732e8aba6033cb51d8
Matheus Lino <matheuslino86@gmail.com.br>
2023-03-23 09:28:10 -0400

    refactor: :recycle: Refactor with modules

--- providers.tf
+++ providers.tf
@@ -4,6 +4,11 @@ terraform {
     aws   = ">=4.0"
     local = ">=2.4.0"
   }
+  backend "s3" {
+    bucket = "myfcbucket-lino"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
